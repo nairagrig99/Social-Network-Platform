65fe109173ec12bc0c58f6e1c30eea96e8e5b0da
Naira Grigoryan <nar.grig.1999@mail.ru>
2024-04-10 00:15:01 +0400

    add validation for registration

---
+++ src/app/auth/interface/auth-user.interface.ts
@@ -0,0 +1,10 @@
+export interface AuthUserInterface {
+  name: string;
+  surname: string;
+  birthOfDate: string;
+  gender: string;
+  country: string;
+  email: string;
+  password: string;
+  rePassword: string
+}
--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -16,6 +16,7 @@ import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 export class InputComponent implements ControlValueAccessor {
 
   @Input() label!: string;
+  @Input() error!: string;
   @Input() placeholder: string = '';
 
   public model!: string;
