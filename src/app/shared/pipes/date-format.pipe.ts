5e4afe9364c5c236431a497709ab171ca214d366
Naira Grigoryan <nar.grig.1999@mail.ru>
2024-04-20 22:16:09 +0400

    bug fix and changed calendar generic method

--- src/app/shared/pipes/date-format.pipe.ts
+++ src/app/shared/pipes/date-format.pipe.ts
@@ -8,11 +8,21 @@ export class DateFormatPipe implements PipeTransform {
   transform(date: any, ...args: any[]): any {
     if (date) {
       const convertToDate: Date = new Date(date);
-      const getDate: number[] = [convertToDate.getDate(), convertToDate.getMonth(), convertToDate.getFullYear()]
-      const strDate: string[] = getDate.join().split('');
+      const day = convertToDate.getDate().toString().split('');
+      const month = (convertToDate.getMonth() + 1).toString().split('');
+      const isTwoNumber = day.length === 1 ? this.changeFormatToTwoNumber(convertToDate.getDate()) : convertToDate.getDate();
+      const isTwoNumberMonth = month.length === 1 ? this.changeFormatToTwoNumber(convertToDate.getMonth() + 1) : convertToDate.getMonth() + 1;
+      const strDate: string[] = [isTwoNumber, isTwoNumberMonth, convertToDate.getFullYear()].join().split('');
+
       const formatDate: string = strDate.map((str) => str.replace(',', '/')).join('')
+
       return formatDate;
     }
     return ''
   }
+
+  private changeFormatToTwoNumber(isTwoNumber: number): string {
+    return isTwoNumber.toString().split('').map((num: string) => num.padStart(2, '0')).join("")
+  }
+
 }
